14f2fd8d2bf9daa50669c36f7c8d928644eda5f8
elymatos <ely.matos@gmail.com>
2020-06-15 15:18:11 -0300

    update filename case

---
+++ apps/mguide/controllers/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+class MainController extends MController {
+
+    public function main() {
+        $this->render();
+    }
+
+    public function menu() {
+        $actions = Manager::getActions('mguide');
+        $primary = [];
+        $secondary = [];
+        $tertiary = [];
+        foreach ($actions as $i => $group) {
+            $primary[$i] = [$i, $group[ACTION_CAPTION], $group[ACTION_PATH]];
+            foreach($group[ACTION_ACTIONS] as $j => $action){
+                $secondary[$i][$j] = [$j, $action[ACTION_CAPTION], $action[ACTION_PATH]];
+                if (is_array($action[ACTION_ACTIONS])) {
+                    foreach($action[ACTION_ACTIONS] as $k => $internalAction){
+                        $tertiary[$i][$j][$k] = [$k, $internalAction[ACTION_CAPTION], $internalAction[ACTION_PATH]];
+                    }
+                }
+            }
+        }
+        $this->data->primary = $primary;
+        $this->data->secondary = $secondary;
+        $this->data->tertiary = $tertiary;
+        $this->render();
+    }
+
+}
